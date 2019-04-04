707a1d9b304b4341126d32a1257802a54d005b67
Brandon Braun <brandonbraun653@gmail.com>
2019-04-03 21:56:32 -0500

    Submodule update. Added more project files.

--- dev/VisualStudio/ArcheFCDevBoard/ArcheFCDevBoard/arche_user_config.hpp
+++ dev/VisualStudio/ArcheFCDevBoard/ArcheFCDevBoard/arche_user_config.hpp
@@ -5,6 +5,13 @@
 /* C++ Includes */
 #include <cstdlib>
 
+/* Chimera Includes */
+#include "chimeraPort.hpp"
+#include <Chimera/types.hpp>
+
+/*------------------------------------------------
+Memory Region Configuration
+------------------------------------------------*/
 static constexpr uint32_t BOOTLOADER_EXPECTED_RGN_SIZE = 64 * 1024;
 
 static constexpr uint32_t BOOTLOADER_START_ADDR = 0x08000000;
@@ -15,6 +22,25 @@ static constexpr uint32_t SYSTEM_IMAGE_START_ADDR = 0x08010000;
 static constexpr uint32_t SYSTEM_IMAGE_END_ADDR   = 0x0807FFFF;
 static constexpr uint32_t SYSTEM_IMAGE_SIZE       = SYSTEM_IMAGE_END_ADDR - SYSTEM_IMAGE_START_ADDR + 1;
 
+/*------------------------------------------------
+Watchdog Configuration
+------------------------------------------------*/
+static constexpr uint32_t USER_WATCHDOG_TIMEOUT_MS = 1000;
+
+/*------------------------------------------------
+Serial Port Configuration
+------------------------------------------------*/
+static constexpr uint8_t USER_SERIAL_CHANNEL = 4;
+
+static constexpr Chimera::GPIO::Port USER_SERIAL_TX_GPIO_PORT   = Chimera::GPIO::Port::PORTA;
+static constexpr Chimera::GPIO::Drive USER_SERIAL_TX_GPIO_DRIVE = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+static constexpr uint32_t USER_SERIAL_TX_GPIO_ALTERNATE         = GPIO_AF8_UART4;
+static constexpr uint8_t USER_SERIAL_TX_GPIO_PIN                = 0;
+
+static constexpr Chimera::GPIO::Port USER_SERIAL_RX_GPIO_PORT   = Chimera::GPIO::Port::PORTA;
+static constexpr Chimera::GPIO::Drive USER_SERIAL_RX_GPIO_DRIVE = Chimera::GPIO::Drive::ALTERNATE_PUSH_PULL;
+static constexpr uint32_t USER_SERIAL_RX_GPIO_ALTERNATE         = GPIO_AF8_UART4;
+static constexpr uint8_t USER_SERIAL_RX_GPIO_PIN                = 1;
 
 /*------------------------------------------------
 Comment to disable the heartbeat functionality
--- dev/VisualStudio/ArcheFCDevBoard/ArcheFCDevBoard/main.cpp
+++ dev/VisualStudio/ArcheFCDevBoard/ArcheFCDevBoard/main.cpp
@@ -10,28 +10,7 @@ int main()
 {
   ChimeraInit();
 
-  Chimera::Threading::addThread(
-    Arche::HeartBeat::threadHeartbeat,
-    Arche::HeartBeat::threadName,
-    Arche::HeartBeat::RTOS_STACK_DEPTH, NULL,
-    Arche::HeartBeat::threadPriority,
-    Arche::HeartBeat::threadHandle);
-
-  Chimera::Threading::addThread(
-    Arche::IOP::iopSerial,
-    Arche::IOP::threadName,
-    Arche::IOP::RTOS_STACK_DEPTH, NULL,
-    Arche::IOP::threadPriority,
-    Arche::IOP::threadHandle);
-
-  Chimera::Threading::addThread(
-    Arche::SYS::sysMain,
-    Arche::SYS::threadName,
-    Arche::SYS::RTOS_STACK_DEPTH, NULL,
-    Arche::SYS::threadPriority,
-    Arche::SYS::threadHandle);
-
-  /* This should never return */
+  Chimera::Threading::addThread( Arche::threads, Arche::numThreads );
   Chimera::Threading::startScheduler();
 
   return 0u;
