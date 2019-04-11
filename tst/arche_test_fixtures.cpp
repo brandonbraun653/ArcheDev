bbd318f9be2e7feaaab3c2c546727f5e9354dac6
Brandon Braun <brandonbraun653@gmail.com>
2019-04-11 12:19:07 -0500

    Submodule update. Added test files from Arche.

---
+++ tst/arche_test_fixtures.cpp
@@ -0,0 +1,11 @@
+/********************************************************************************
+ *  File Name:
+ *    arche_test_fixtures.cpp
+ *
+ *  Description:
+ *    Implements various test fixtures needed for Arche
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#include "arche_test_fixtures.hpp"
---
+++ tst/arche_test_fixtures.hpp
@@ -0,0 +1,21 @@
+/********************************************************************************
+ *  File Name:
+ *    arche_test_fixtures.hpp
+ *
+ *  Description:
+ *    Defines various fixtures for Arche
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once
+#ifndef ARCHE_TEST_FIXTURES_HPP
+#define ARCHE_TEST_FIXTURES_HPP
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+
+
+#endif /* !ARCHE_TEST_FIXTURES_HPP */
